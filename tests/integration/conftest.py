bac1aae24988246889e506d27cba1ecfb89aff1e
Lisa Pichler <lisa.pichler@tribe29.com>
2022-10-06 18:21:48 +0200

    Integration tests: make container images configurable
    Change-Id: I733a5208b3af0dee39efa1bebe15145794370615

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -7,17 +7,20 @@
 
 """Conftest file for integration tests"""
 
+from typing import Optional
+
 import pytest
 from _pytest.config.argparsing import Parser
 
 from tests.integration.common_helpers import tcp_session
 from tests.integration.kube_api_helpers import APIServer
 
+# pylint: disable=missing-function-docstring
+
 
 def pytest_addoption(
     parser: Parser,
 ) -> None:
-    # pylint: disable=missing-function-docstring
     parser.addoption(
         "--cluster-endpoint",
         action="store",
@@ -36,6 +39,32 @@ def pytest_addoption(
         action="store",
         help="The name of the checkmk_kube_agent chart",
         default="deploy/charts/checkmk",
+    )
+    parser.addoption(
+        "--image-registry",
+        help="Registry path where the test images can be found",
+        default="checkmk",
+    )
+    parser.addoption(
+        "--image-pull-secret-name",
+        help=(
+            "Name of the image pull secret that exists in the 'default' "
+            "namespace of the Kubernetes cluster"
+        ),
+    )
+    parser.addoption(
+        "--collector-image-name",
+        help="Name of the Kubernetes collector image",
+        default="kubernetes-collector",
+    )
+    parser.addoption(
+        "--cadvisor-image-name",
+        help="Name of the cAdvisor image",
+        default="cadvisor-patched",
+    )
+    parser.addoption(
+        "--image-tag",
+        help="Image tag to use",
     )
 
 
@@ -70,3 +99,30 @@ def fixture_api_server(cluster_endpoint: str, cluster_token: str) -> APIServer:
         session=tcp_session(headers={"authorization": f"Bearer {cluster_token}"}),
         cluster_endpoint=cluster_endpoint,
     )
+
+
+@pytest.fixture(scope="session")
+def image_registry(request: pytest.FixtureRequest) -> str:
+    return request.config.getoption("image_registry")
+
+
+@pytest.fixture(scope="session")
+def image_pull_secret_name(request: pytest.FixtureRequest) -> Optional[str]:
+    """Image pull secrets are needed when images are retrieved from a private
+    registry."""
+    return request.config.getoption("image_pull_secret_name")
+
+
+@pytest.fixture(scope="session")
+def collector_image_name(request: pytest.FixtureRequest) -> str:
+    return request.config.getoption("collector_image_name")
+
+
+@pytest.fixture(scope="session")
+def cadvisor_image_name(request: pytest.FixtureRequest) -> str:
+    return request.config.getoption("cadvisor_image_name")
+
+
+@pytest.fixture(scope="session")
+def image_tag(request: pytest.FixtureRequest) -> Optional[str]:
+    return request.config.getoption("image_tag")
