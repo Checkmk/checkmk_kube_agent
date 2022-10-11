16791a0d19ceca7f6846e22b9ff72b89ec789a62
Frans Fürst <frans.fuerst@tribe29.com>
2022-10-11 11:11:59 +0200

    refactor docker image use
    Change-Id: I62e91abc53f882ee26961b5c7e7660d2a6a49661

--- ci/jenkins/build-kube-agent.groovy
+++ ci/jenkins/build-kube-agent.groovy
@@ -32,31 +32,35 @@ def main() {
              "GIT_COMMITTER_NAME=Checkmk release system",
              "GIT_COMMITTER_EMAIL=feedback@check-mk.org"]) {
 
+        def kube_agent_github_repo = "tribe29/checkmk_kube_agent";
+        def kube_agent_github_url = "https://github.com/${kube_agent_github_repo}";
+        def github_pages_branch = "gh-pages";
+        def ci_image_name = "checkmk-kube-agent-ci";
+
         def branch = scm.branches[0].name;
         def version = params.VERSION;
         def method = params.METHOD;
         def is_release_build = (method != "daily");
         def push_to_dockerhub = params.PUSH_TO_DOCKERHUB;
         
-        print("VERSION:           ${params.VERSION}");
-        print("METHOD:            ${params.METHOD}");
-        print("PUSH_TO_DOCKERHUB: ${params.PUSH_TO_DOCKERHUB}");
-        print("branch:            ${branch}");
-        print("version:           ${version}");
-        print("method:            ${method}");
-        print("push_to_dockerhub: ${push_to_dockerhub}");
-        print("is_release_build:  ${is_release_build}");
-
-        def kube_agent_github_repo = "tribe29/checkmk_kube_agent";
-        def kube_agent_github_url = "https://github.com/${kube_agent_github_repo}";
-        def github_pages_branch = "gh-pages";
-        def ci_image_name = "checkmk-kube-agent-ci";
+        print("VERSION:                 ${params.VERSION}");
+        print("METHOD:                  ${params.METHOD}");
+        print("PUSH_TO_DOCKERHUB:       ${params.PUSH_TO_DOCKERHUB}");
+        print("branch:                  ${branch}");
+        print("version:                 ${version}");
+        print("method:                  ${method}");
+        print("kube_agent_github_repo:  ${kube_agent_github_repo}");
+        print("kube_agent_github_url:   ${kube_agent_github_url}");
+        print("github_pages_branch:     ${github_pages_branch}");
+        print("ci_image_name:           ${ci_image_name}");
+        print("push_to_dockerhub:       ${push_to_dockerhub}");
+        print("is_release_build:        ${is_release_build}");
 
         validate_parameters_and_branch(method, version, branch);
 
         def git_commit_id;
         // TODO: at least consolidate in this repo...
-        def DOCKER_GROUP_ID = sh(script: "getent group docker | cut -d: -f3", returnStdout: true);
+        def docker_group_id = sh(script: "getent group docker | cut -d: -f3", returnStdout: true);
         def helm_repo_index_file = "index.yaml";
         def STAGE_PUSH_IMAGES = 'Push Images';
 
@@ -66,13 +70,16 @@ def main() {
         }
 
         stage('Build CI Image') {
-            docker.build(ci_image_name, "--network=host -f docker/ci/Dockerfile .");
+            def x = docker.build(ci_image_name, "--network=host -f docker/ci/Dockerfile .");
+            print("x: ${x}");
         }
+        
+        def ci_image = docker.image(ci_image_name);
 
         if (is_release_build) {
             stage('Calculate Version') {
                 if (method != "rebuild_version") {
-                    docker.image(ci_image_name).inside("--entrypoint=") {
+                    ci_image.inside("--entrypoint=") {
                         version = run_in_ash("METHOD=${method} make print-bumped-version", true).toString().trim();
                     }
                 }
@@ -81,7 +88,7 @@ def main() {
             stage("Create or switch to tag") {
                 withCredentials([sshUserPrivateKey(credentialsId: "release", keyFileVariable: 'keyfile')]) {
                     withEnv(["GIT_SSH_COMMAND=ssh -o \"StrictHostKeyChecking no\" -i ${keyfile} -l release"]) {
-                        docker.image(ci_image_name).inside("--entrypoint=") {
+                        ci_image.inside("--entrypoint=") {
                             // FIXME run_in_ash("ci/jenkins/scripts/tagging.sh ${version} ${method} ${branch}")
                         }
                     }
@@ -94,7 +101,7 @@ def main() {
             conditional_stage("Push to github", method != "rebuild_version") {
                 withCredentials([sshUserPrivateKey(credentialsId: "ssh_private_key_lisa_github", keyFileVariable: 'keyfile')]) {
                     withEnv(["GIT_SSH_COMMAND=ssh -o \"StrictHostKeyChecking no\" -i ${keyfile}"]) {
-                        docker.image(ci_image_name).inside("--entrypoint=") {
+                        ci_image.inside("--entrypoint=") {
                             // FIXME  run_in_ash("git push --tags github ${branch}")
                         }
                     }
@@ -103,14 +110,14 @@ def main() {
         }
 
         stage("Build source and wheel package") {
-            docker.image(ci_image_name).inside("--entrypoint=") {
+            ci_image.inside("--entrypoint=") {
                 run_in_ash("make dist");
             }
         }
 
         stage("Build Images") {
             def docker_image_tag = determine_docker_tag(is_release_build, version);
-            docker.image(ci_image_name).inside("-v /var/run/docker.sock:/var/run/docker.sock --group-add=${DOCKER_GROUP_ID} --entrypoint=") {
+            ci_image.inside("-v /var/run/docker.sock:/var/run/docker.sock --group-add=${docker_group_id} --entrypoint=") {
                 run_in_ash("make DOCKER_IMAGE_TAG=${docker_image_tag} release-image");
             }
             conditional_stage("Push Images", params.PUSH_TO_DOCKERHUB) {
@@ -119,7 +126,7 @@ def main() {
                         credentialsId: '11fb3d5f-e44e-4f33-a651-274227cc48ab',
                         passwordVariable: 'DOCKER_PASSPHRASE',
                         usernameVariable: 'DOCKER_USERNAME')]) {
-                    docker.image(ci_image_name).inside("-v /var/run/docker.sock:/var/run/docker.sock --group-add=${DOCKER_GROUP_ID} --entrypoint=") {
+                    ci_image.inside("-v /var/run/docker.sock:/var/run/docker.sock --group-add=${docker_group_id} --entrypoint=") {
                         run_in_ash('echo \"${DOCKER_PASSPHRASE}\" | docker login -u ${DOCKER_USERNAME} --password-stdin');
                         // FIXME run_in_ash("make DOCKER_IMAGE_TAG=${docker_image_tag} push-images");
                     }
@@ -134,12 +141,12 @@ def main() {
                     passwordVariable: 'GH_TOKEN',
                     usernameVariable: "GH_USER")]) {
                 conditional_stage("Delete github release", method == "rebuild_version") {
-                    docker.image(ci_image_name).inside("--entrypoint=") {
+                    ci_image.inside("--entrypoint=") {
                         // FIXME run_in_ash("gh release delete --repo ${kube_agent_github_repo} -y v${version}")
                     }
                 }
                 stage("Create github release, upload helm chart artifact") {
-                    docker.image(ci_image_name).inside("--entrypoint=") {
+                    ci_image.inside("--entrypoint=") {
                         // Note: see more details on the "gh" tool in the Dockerfile of the CI image.
                         // FIXME 
                         /*
@@ -160,7 +167,7 @@ def main() {
                 sh("git checkout ${github_pages_branch}");
                 withCredentials([sshUserPrivateKey(credentialsId: "release", keyFileVariable: 'keyfile')]) {
                     withEnv(["GIT_SSH_COMMAND=ssh -o \"StrictHostKeyChecking no\" -i ${keyfile} -l release"]) {
-                        docker.image(ci_image_name).inside("--entrypoint=") {
+                        ci_image.inside("--entrypoint=") {
                             // FIXME 
                             /*
                             run_in_ash("""
@@ -176,7 +183,7 @@ def main() {
                 // This can be deleted if the push to github can be triggered some other way (see CMK-9584)
                 withCredentials([sshUserPrivateKey(credentialsId: "ssh_private_key_lisa_github", keyFileVariable: 'keyfile')]) {
                     withEnv(["GIT_SSH_COMMAND=ssh -o \"StrictHostKeyChecking no\" -i ${keyfile}"]) {
-                        docker.image(ci_image_name).inside("--entrypoint=") {
+                        ci_image.inside("--entrypoint=") {
                             run_in_ash("git checkout ${github_pages_branch}");
                             // FIXME run_in_ash("git push github ${github_pages_branch}");
                             sh("git checkout ${branch}");
