1e384f891aa72930fbed7de6839b8caa48d32631
Lisa Pichler <lisa.pichler@tribe29.com>
2023-04-26 13:27:56 +0200

    CI: build script: add validate credentials step
    Change-Id: Id72ca675f75017c9e27b5e168b54d937ba78bc3e

--- ci/jenkins/build-kube-agent.groovy
+++ ci/jenkins/build-kube-agent.groovy
@@ -86,6 +86,12 @@ def main(BRANCH, METHOD, VERSION) {
         }
         docker.build(CI_IMAGE, "--network=host -f docker/ci/Dockerfile .");
 
+        stage('Validate Github credentials') {
+            build(
+            job: "./validate-release-credentials",
+            )
+        }
+
         if (RELEASE_BUILD) {
             stage('Calculate Version') {
                 if (METHOD != "rebuild_version") {
