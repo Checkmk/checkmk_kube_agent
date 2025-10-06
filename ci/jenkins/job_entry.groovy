5e20e4f736d62252c44ed622891acfeb0d9f7032
Niko Wenselowski <niko.wenselowski@checkmk.com>
2025-10-06 17:02:24 +0200

    CI: Backport job entry file
    The CI config for the jobs has ci/jenkins/job_entry.groovy configured.
    This file only exists on the main branch, not on this one.
    
    This backport aims at fixing problems where the entry file isn't found.
    
    Change-Id: I833e7074d367e0fad087923b64b7ed4c0a30db12

---
+++ ci/jenkins/job_entry.groovy
@@ -0,0 +1,33 @@
+#!groovy
+
+/// file: job_entry.groovy
+
+/// This is the branch specific checkmk_kube_agent main entry point. It exists to
+/// avoid redundant code in the actual job definition files and to be able
+/// to provide a standard environment for all checkmk_kube_agent jobs
+
+def main(job_definition_file) {
+    sh_output = { cmd ->
+        try {
+            return sh(script: cmd, returnStdout: true).trim();
+        } catch (Exception exc) {
+            print("WARNING: Executing ${cmd} returned non-zero: ${exc}");
+        }
+        return "";
+    };
+
+    ash = { cmd ->
+        sh(script: "#!/bin/ash\n${cmd}", returnStdout: false);
+    }
+
+    ash_output = { cmd ->
+        return sh(script: "#!/bin/ash\n${cmd}", returnStdout: true).toString().trim();
+    }
+
+    /// TODO: error handling!
+    dir("${checkout_dir}") {
+        load("${checkout_dir}/${job_definition_file}").main();
+    }
+}
+
+return this;
